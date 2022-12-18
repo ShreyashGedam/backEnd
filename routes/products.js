dcadc06a537e9234c9fc22bcb9d7032e99e7d143
Shreyash Gedam <shreyashakgedam@gmail.com>
2022-12-18 18:50:13 +0530

    Brand Sorting
    Sorting all key with single value in url

--- routes/products.js
+++ routes/products.js
@@ -4,21 +4,30 @@ const productRouter = Router()
 
 productRouter.get("", async (req, res) => {
 
-    var x = req.query
-    const tasks = await ProductsModel.find(x)
+    var obj = req.query
 
+    if (req.query.name != undefined) {
+        var mname = req.query.name.split(":")
+        console.log(mname)
+        var obj = {
+            ...req.query,
+            name: mname
+        }
+    }
+
+    const tasks = await ProductsModel.find(obj)
+    // const tasks = await ProductsModel.find({ name: { $in: filter.name } })
     return res.send(tasks)
 })
- 
+
 productRouter.get("/:userId/:productId", async (req, res) => {
     // const userId = req.params.userId
     const product = { _id: req.params.productId }
 
     const tasks = await ProductsModel.find(product)
     // console.log(tasks)
-    return res.send(tasks)      
-
+    return res.send(tasks)
 })
 
- 
+
 module.exports = productRouter     
